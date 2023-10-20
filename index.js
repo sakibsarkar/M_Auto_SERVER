e0a4dee7f3dffbc1fcf0b8960f0888e6dceb1892
sakibsarkar <sakibsarkar707@gmail.com>
2023-10-20 19:14:03 +0600

    error messege deleted

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ app.use(express.json())
 
 
 // error handling
-app.use((req, res, next) => {
-  const error = new Error('Not Found');
-  error.status = 404;
-  next(error);
-});
+// app.use((req, res, next) => {
+//   const error = new Error('Not Found');
+//   error.status = 404;
+//   next(error);
+// });
 
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message
+//     }
+//   });
+// });
 
 
 
